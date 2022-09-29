a5dca9bca192e223c282a0686634433be71f6740
Loic Kami <ruvice.kami@gmail.com>
2022-09-29 08:49:59 +0100

    - Updated footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,14 +19,17 @@ const Footer: React.FC<{}> = ({ }) => {
         <footer className="px-4 pt-8 text-gray-800">
             <div className="container flex flex-wrap items-center justify-center mx-auto space-y-4 sm:justify-between sm:space-y-0">
                 <div className="flex flex-row flex-wrap pr-3 space-x-4 sm:space-x-8">
-                    <div className="flex items-center justify-center flex-shrink-0 mx-auto h-24 w-48 relative">
-                        <Image
-                            layout="fill"
-                            src={logoImage}
-                            className="object-contain"
-                            alt="Logo - Taroungamograph by Tarounga Mbaye "
-                        />
-                    </div>
+                    <Link href="/" aria-label="Back to Gesmax by Bogital homepage" className="flex items-center justify-center flex-shrink-0">
+                        <div className="mx-auto h-24 w-48 relative cursor-pointer">
+                            <Image
+                                layout="fill"
+                                src={logoImage}
+                                className="object-contain"
+                                alt="Logo - Taroungamograph by Tarounga Mbaye "
+                            />
+                        </div>
+                    </Link>
+
                     <ul className="flex flex-wrap items-center space-x-4 sm:space-x-8">
                         {
                             routes.map(route => (
