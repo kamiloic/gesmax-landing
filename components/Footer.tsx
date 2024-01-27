43a1de61c10232d8aa3994ce99f671cee02df162
Loic Kami <ruvice.kami@gmail.com>
2024-01-27 21:33:55 +0100

    Update Footer layout and add @2024 Bogital

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,7 +17,7 @@ const Footer: React.FC<{}> = ({ }) => {
 
     return (
         <footer className="pt-8 text-gray-800">
-            <div className="container max-w-7xl flex flex-wrap items-center justify-center mx-auto space-y-4 sm:justify-between sm:space-y-0">
+            <div className="px-4 py-16 sm:px-6 lg:px-8    container max-w-7xl flex flex-wrap items-center justify-center mx-auto space-y-4 sm:justify-between sm:space-y-0   ">
                 <div className="flex flex-row flex-wrap pr-3 space-x-4 sm:space-x-8">
                     <Link href="/" aria-label="Back to Gesmax by Bogital homepage" className="flex items-center justify-center flex-shrink-0">
                         <div className="mx-auto h-24 w-48 relative cursor-pointer">
@@ -56,7 +56,11 @@ const Footer: React.FC<{}> = ({ }) => {
                     </li>
                 </ul> */}
             </div>
-            <p className="text-xs text-center pt-4 p-2 md:pt-2">Coded with ❤️ by <a href="https://bogital.com">Bogital</a></p>
+            
+            <div className="flex justify-between px-4 sm:px-6 lg:px-8">
+                <p className="text-xs text-center pt-4 p-2 md:pt-2">Powered with ❤️ by <a href="https://bogital.com" className="text-primary" target="_blank">Bogital</a></p>
+                <p className="text-xs text-center px-2 hover:text-primary"><a href="#@2024" target="_blank">@2024 </a>Bogital</p>
+            </div>
         </footer>
     )
 }
