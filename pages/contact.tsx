96dacf0bc489d3fa7500a577d78a3d6993f4cb02
Loic Kami <ruvice.kami@gmail.com>
2024-01-28 02:04:56 +0100

    Swapped form and contact positions

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -20,39 +20,6 @@ const Contact: NextPage<Props> = ({ data }) => {
         {/* <section className="min-h-screen bg-gradient-to-r from-primary via-blue-800 to-blue-900 dark:from-gray-700 dark:via-gray-800 dark:to-gray-900"> */}
         <div className="container flex flex-col min-h-screen max-w-7xl px-6 py-12 mx-auto">
           <div className="flex-1 lg:flex lg:items-center lg:-mx-6">
-            <div className="text-white lg:w-1/2 lg:mx-6">
-              <h1 className="text-3xl font-semibold capitalize lg:text-5xl">{s.contact}</h1>
-
-              <div className="mt-6 space-y-8 md:mt-8">
-                <p className="flex items-start -mx-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-2 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-                  </svg>
-
-                  <span className="mx-2 text-white truncate w-72">
-                    Total melen, Yaounde
-                  </span>
-                </p>
-
-                <p className="flex items-start -mx-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-2 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
-                  </svg>
-
-                  <span className="mx-2 text-white truncate w-72">(237) 693-369-322</span>
-                </p>
-
-                <p className="flex items-start -mx-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-2 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                  </svg>
-
-                  <span className="mx-2 text-white truncate w-72">gesmax@bogital.com</span>
-                </p>
-              </div>
-            </div>
-
 
             <div className="mt-8 lg:w-1/2 lg:mx-6">
               {
@@ -94,6 +61,39 @@ const Contact: NextPage<Props> = ({ data }) => {
                     {s.send}
                   </button>
                 </form>
+              </div>
+            </div>
+            
+            <div className="text-white lg:w-1/2 lg:mx-6">
+              <h1 className="text-3xl font-semibold capitalize lg:text-5xl">{s.contact}</h1>
+
+              <div className="mt-6 space-y-8 md:mt-8">
+                <p className="flex items-start -mx-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-2 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                  </svg>
+
+                  <span className="mx-2 text-white truncate w-72">
+                    Total melen, Yaounde
+                  </span>
+                </p>
+
+                <p className="flex items-start -mx-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-2 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                  </svg>
+
+                  <span className="mx-2 text-white truncate w-72">(237) 693-369-322</span>
+                </p>
+
+                <p className="flex items-start -mx-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-2 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                  </svg>
+
+                  <span className="mx-2 text-white truncate w-72">gesmax@bogital.com</span>
+                </p>
               </div>
             </div>
 
