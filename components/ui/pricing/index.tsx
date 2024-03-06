d6e4166a627e8bf6d31b49fe0c387b9b1b90292c
Loic Kami <ruvice.kami@gmail.com>
2024-03-06 15:02:59 +0100

    Update strings

--- components/ui/pricing/index.tsx
+++ components/ui/pricing/index.tsx
@@ -1,4 +1,4 @@
-import type { GetStaticProps, NextPage } from 'next'
+import { GetStaticProps, NextPage } from 'next'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useMemo, useState } from 'react'
@@ -17,12 +17,12 @@ const PricingComponent: NextPage<Props> = ({ }) => {
 	const [userCount, setUserCount] = useState<number>(3);
 	const { locale } = useRouter()
 	const isFr = useMemo(() => (locale || '').toLowerCase().includes('fr'), [locale])
-	const requestDemoText = isFr ? 'Demander une démo' : 'Request a demo'
+	const { strings } = useStrings(isFr);
 
 	const RequestDemo = () => (
 		<Link href='contact'>
 			<span className="inline-block w-full px-5 py-3 font-semibold tracking-wider cursor-pointer text-center rounded bg-primary text-gray-900">
-				{requestDemoText}
+				{strings.requestDemoText}
 			</span>
 		</Link>
 	)
@@ -46,7 +46,7 @@ const PricingComponent: NextPage<Props> = ({ }) => {
 		<section className="py-20 bg-gradient-to-r from-primary via-blue-800 to-blue-900 text-gray-100">
 			<div className="container max-w-7xl px-4 mx-auto">
 				<div className="max-w-2xl mx-auto mb-16 text-center">
-					<span className="font-bold tracking-wider uppercase text-primary">Pricing</span>
+					<span className="font-bold tracking-wider uppercase text-primary">{strings.pricingTitle}</span>
 					<p className='text-2xl'>
 						<input
 							className='bg-transparent w-16 text-center'
@@ -56,116 +56,99 @@ const PricingComponent: NextPage<Props> = ({ }) => {
 							min={3}
 							max={200}
 						/>
-						<h2 className="font-bold lg:text-5xl inline-block">users?</h2>
+						<h2 className="font-bold lg:text-5xl inline-block">{strings.usersQuestion}</h2>
 					</p>
 				</div>
 
 
 				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-					{/* Pricing section 1 */}
-					<div className="flex w-full mb-8 sm:px-4 md:w-full lg:mb-0">
-						<div className="flex flex-col p-6 space-y-6 rounded shadow sm:p-8 bg-gray-900">
-							<div className="space-y-2">
-								<h4 className="text-2xl font-bold">Free</h4>
-								<span className="text-4xl font-bold">
-									<CurrencyFormat amount={0} />
-								</span>
-								{/* <span className="text-6xl font-bold">Free</span> */}
+					{strings.pricingSections.map((section, index) => (
+						<div className="flex w-full mb-8 sm:px-4 md:w-full lg:mb-0" key={index}>
+							<div className="flex flex-col p-6 space-y-6 rounded shadow sm:p-8 bg-gray-900">
+								<div className="space-y-2">
+									<h4 className="text-2xl font-bold">{section.title}</h4>
+									<span className="text-4xl font-bold">
+										<CurrencyFormat amount={section.price * monthlyFee} />
+									</span>
+								</div>
+								<ul className="flex-1 mb-6 text-gray-400">
+									{section.features.map((feature, index) => (
+										<li className="flex items-center space-x-2" key={index}>
+											<SVGTickGray />
+											<span>{feature}</span>
+										</li>
+									))}
+								</ul>
+								<RequestDemo />
 							</div>
-							{/* <p className="mt-3 leading-relaxed text-gray-400">Etiam ac convallis enim, eget euismod dolor.</p> */}
-							<ul className="flex-1 mb-6 text-gray-400">
-								<li className="flex items-center space-x-2">
-									<SVGTickGray />
-									<span>30 Orders per month</span>
-								</li>
-								<li className="flex items-center space-x-2">
-									<SVGTickGray />
-									<span>Limited support</span>
-								</li>
-							</ul>
-							<RequestDemo />
 						</div>
-					</div>
-
-					{/* Pricing section 2 */}
-					<div className="flex w-full mb-8 sm:px-4 md:w-full lg:mb-0">
-						<div className="flex flex-col p-6 space-y-6 rounded shadow sm:p-8 bg-gray-900">
-							<div className="space-y-2">
-								<h4 className="text-2xl font-bold">1 Month</h4>
-								<span className="text-4xl font-bold">
-									<CurrencyFormat amount={monthlyFee} />
-								</span>
-								{/* <span className="text-6xl font-bold">Free</span> */}
-							</div>
-							{/* <p className="mt-3 leading-relaxed text-gray-400">Etiam ac convallis enim, eget euismod dolor.</p> */}
-							<ul className="flex-1 mb-6 text-gray-400">
-								<li className="flex items-center space-x-2">
-									<SVGTickGray />
-									<span>All features available</span>
-								</li>
-								<li className="flex items-center space-x-2">
-									<SVGTickGray />
-									<span>24/24 7/7 support</span>
-								</li>
-							</ul>
-							<RequestDemo />
-						</div>
-					</div>
-
-					{/* Pricing section 3 */}
-					<div className="flex w-full mb-8 sm:px-4 md:w-full lg:mb-0">
-						<div className="p-6 space-y-6 rounded shadow sm:p-8 bg-gray-900">
-							<div className="space-y-2">
-								<h4 className="text-2xl font-bold">1 Year</h4>
-								<span className="text-4xl font-bold">
-									<CurrencyFormat amount={11 * monthlyFee} />
-								</span>
-							</div>
-							{/* <p className="leading-relaxed text-gray-400">Phasellus ultrices bibendum nibh in vehicula.</p> */}
-							<ul className="space-y-2 text-gray-400">
-								<li className="flex items-center space-x-2">
-									<SVGTickGray />
-									<span>All features available</span>
-								</li>
-								<li className="flex items-center space-x-2">
-									<SVGTickGray />
-									<span>24/24 7/7 support</span>
-								</li>
-							</ul>
-							<RequestDemo />
-						</div>
-					</div>
-
-					{/* Pricing section 4 */}
-					<div className="flex w-full mb-8 sm:px-4 md:w-full lg:mb-0">
-						<div className="p-6 space-y-6 rounded shadow sm:p-8 bg-gray-900">
-							<div className="space-y-2">
-								<h4 className="text-2xl font-bold">3 Years</h4>
-								<span className="text-4xl font-bold">
-									<CurrencyFormat amount={30 * monthlyFee} />
-								</span>
-							</div>
-							{/* <p className="leading-relaxed text-gray-400">Phasellus ultrices bibendum nibh in vehicula.</p> */}
-							<ul className="space-y-2 text-gray-400">
-								<li className="flex items-center space-x-2">
-									<SVGTickGray />
-									<span>All features available</span>
-								</li>
-								<li className="flex items-center space-x-2">
-									<SVGTickGray />
-									<span>24/24 7/7 support</span>
-								</li>
-							</ul>
-							<RequestDemo />
-						</div>
-					</div>
+					))}
 				</div>
-
 			</div>
 		</section>
 	)
 }
 
+const strings = {
+	'en': {
+		'pricingTitle': 'Pricing',
+		'usersQuestion': 'users?',
+		'requestDemoText': 'Request a demo',
+		'pricingSections': [
+			{
+				'title': 'Free',
+				'price': 0,
+				'features': ['30 Orders per month', 'Limited support']
+			},
+			{
+				'title': '1 Month',
+				'price': 1,
+				'features': ['All features available', '24/24 7/7 support']
+			},
+			{
+				'title': '1 Year',
+				'price': 11,
+				'features': ['All features available', '24/24 7/7 support']
+			},
+			{
+				'title': '3 Years',
+				'price': 30,
+				'features': ['All features available', '24/24 7/7 support']
+			}
+		]
+	},
+	'fr': {
+		'pricingTitle': 'Tarification',
+		'usersQuestion': 'utilisateurs?',
+		'requestDemoText': 'Demander une démo',
+		'pricingSections': [
+			{
+				'title': 'Gratuit',
+				'price': 0,
+				'features': ['30 Commandes par mois', 'Support limité']
+			},
+			{
+				'title': '1 Mois',
+				'price': 1,
+				'features': ['Toutes les fonctionnalités disponibles', '24/24 7/7 support']
+			},
+			{
+				'title': '1 An',
+				'price': 11,
+				'features': ['Toutes les fonctionnalités disponibles', '24/24 7/7 support']
+			},
+			{
+				'title': '3 Ans',
+				'price': 30,
+				'features': ['Toutes les fonctionnalités disponibles', '24/24 7/7 support']
+			}
+		]
+	},
+};
+
+const useStrings = (isFr: boolean) => {
+	return { strings: isFr ? strings['fr'] : strings['en'] };
+}
 
 export const getStaticProps: GetStaticProps = async (context) => {
 	const data: any[] = []
@@ -173,6 +156,5 @@ export const getStaticProps: GetStaticProps = async (context) => {
 		props: { data }
 	};
 }
-
 
 export default PricingComponent
