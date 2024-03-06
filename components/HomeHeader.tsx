2d6321ec2c74059e149cdcc09d9905735ddf748f
Loic Kami <ruvice.kami@gmail.com>
2024-03-06 15:45:01 +0100

    Update HomeHeader

--- components/HomeHeader.tsx
+++ components/HomeHeader.tsx
@@ -1,15 +1,14 @@
 import Link from "next/link"
-import { useMemo, useState } from "react"
 import { Navigation } from "./navigation"
 
-
 interface Props {
-    active: string
-    locale: string
+    active: string;
+    locale: string;
 }
 
 const HomeHeader: React.FC<Props> = ({ active, locale }) => {
-    const isFrench = useMemo(() => locale.toLowerCase().includes('fr'), [locale])
+    const strings = useStrings(locale);
+
     return (
         <header className="flex flex-col justify-between container max-w-7xl mx-auto bg-cover">
 
@@ -19,23 +18,19 @@ const HomeHeader: React.FC<Props> = ({ active, locale }) => {
             <section className="bg-white text-gray-900">
                 <div className="container flex flex-col justify-center sm:py-6 lg:py-12 lg:flex-row lg:justify-items-stretch py-16 max-w-7xl ">
                     <div className="flex flex-1 flex-col justify-center lg:justify-start p-6 text-center rounded-sm lg:text-left lg:max-w-md xl:max-w-lg">
-                        <h1 className="text-5xl font-bold leading-none sm:text-5xl">A
-                            <span className="text-primary"> Magical</span> new way of managing your business
-                        </h1>
-                        <p className="mt-6 mb-8 text-lg sm:mb-12"><i>Gesmax by Bogital</i> is available on all plateforms:
-                            <br className="hidden md:inline lg:hidden" /> PC, Android, iOs and web
-                        </p>
+                        <h1 className="text-5xl font-bold leading-none sm:text-5xl" dangerouslySetInnerHTML={{
+                            __html: strings.title
+                        }} />
+                        <p className="mt-6 mb-8 text-lg sm:mb-12">{strings.subtitle}</p>
                         <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-                            <button onClick={(e) => {
-                                window.open("https://gesmaxapp.vercel.app", "_blank")
-                            }}>
+                            <button onClick={() => window.open("https://gesmaxapp.vercel.app", "_blank")}>
                                 <span className="px-8 py-3 text-lg font-semibold rounded bg-primary text-white cursor-pointer">
-                                    {isFrench ? 'Démo' : 'Request a demo'}
+                                    {strings.demo}
                                 </span>
                             </button>
                             <Link href='/pricing'>
                                 <span className="px-8 py-3 text-lg font-semibold text-primary cursor-pointer">
-                                    {isFrench ? 'Coûts' : 'See pricing'}
+                                    {strings.seePricing}
                                 </span>
                             </Link>
                         </div>
@@ -48,6 +43,24 @@ const HomeHeader: React.FC<Props> = ({ active, locale }) => {
 
         </header>
     )
+}
+
+const useStrings = (locale: string) => {
+    const isFr = locale.toLowerCase().includes('fr');
+    return {
+        en: {
+            title: "A <span style='color:#3b82f6'>Magical</span> new way of managing your business",
+            subtitle: "For seamless business management anytime, anywhere: Gesmax by Bogital is accessible on all major app stores and platforms – including PC, Android, iOS, and web – ensuring convenience and flexibility for your workflow.",
+            demo: "Request a demo",
+            seePricing: "See pricing"
+        },
+        fr: {
+            title: "Une nouvelle façon <span style='color:#3b82f6'>Magique</span> de gérer votre entreprise",
+            subtitle: "Pour une gestion commerciale fluide, où que vous soyez : Gesmax par Bogital est disponible sur toutes les grandes plateformes et magasins d'applications, notamment sur PC, Android, iOS et le web, garantissant ainsi une commodité et une flexibilité optimales pour votre flux de travail.",
+            demo: "Démo",
+            seePricing: "Voir les prix"
+        }
+    }[isFr ? 'fr' : 'en'];
 }
 
 export default HomeHeader
