e6e438939cde692795031a5c40f78f279c0f3b32
Loic Kami <ruvice.kami@gmail.com>
2024-03-06 15:27:10 +0100

    Update navigation strings

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,59 +1,49 @@
 import Link from "next/link"
-import { useMemo, useState } from "react"
-
+import { useState } from "react"
 
 interface Props {
-	active: string
-	locale: string
+	active: string;
+	locale: string;
 }
-
 
 /** Navigation component */
 export const Navigation: React.FC<Props> = ({ active, locale }) => {
-
-	const isFrench = useMemo(() => locale.toLowerCase().includes('fr'), [locale])
-	const [isActive, setIsAvtice] = useState(false)
+	const strings = useStrings(locale);
+	const [isActive, setIsActive] = useState(false);
 
 	const activeLink = (routeName: string) => {
-		// TODO: Use classnames from npm
-		if (!active) return 'flex items-center px-4 -mb-1 border-b-2 border-transparent hover:text-primary transition-colors cursor-pointer'
-		const isActive = active.toLowerCase().includes(routeName.toLowerCase())
-		if (isActive) return 'flex cursor-pointer items-center px-4 -mb-1 border-b-2 link-active'
-		return 'flex items-center px-4 -mb-1 border-b-2 border-transparent hover:text-primary transition-colors cursor-pointer'
+		if (!active) return 'flex items-center px-4 -mb-1 border-b-2 border-transparent hover:text-primary transition-colors cursor-pointer';
+		const isActive = active.toLowerCase().includes(routeName.toLowerCase());
+		if (isActive) return 'flex cursor-pointer items-center px-4 -mb-1 border-b-2 link-active';
+		return 'flex items-center px-4 -mb-1 border-b-2 border-transparent hover:text-primary transition-colors cursor-pointer';
 	}
 
 	const toggleMenu = () => {
-		setIsAvtice(v => !v)
+		setIsActive(!isActive);
 	}
-
 
 	return (
 		<nav className="py-4 text-gray-800">
 			<div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
 				<div className="flex justify-between h-16 ">
-					<Link href="/" aria-label="Back to Gesmax by Bogital homepage" className="flex items-center p-2">
+					<Link href="/" aria-label={strings.backToHomepage} className="flex items-center p-2">
 						<div className="flex items-center cursor-pointer z-50">
 							<div className="h-16 w-auto relative">
-								<img src="/images/logo-lg.png" alt="Gesmax by bogital Logo" className="object-contain h-16 sm:h-16 lg:h-16 xl:h-16 2xl:h-16" />
+								<img src="/images/logo-lg.png" alt={strings.logoAlt} className="object-contain h-16 sm:h-16 lg:h-16 xl:h-16 2xl:h-16" />
 							</div>
 						</div>
 					</Link>
-					<ul className={"items-stretch hidden space-x-3 md:flex"}>
+					<ul className="items-stretch hidden space-x-3 md:flex">
 						<li className={activeLink('pricing')}>
 							<Link href='/pricing' className="flex items-center -mb-1">
-								<span className="">
-									{isFrench ? 'Coûts' : 'Pricing'}
-								</span>
+								<span className="">{strings.pricing}</span>
 							</Link>
 						</li>
 						<li className={activeLink('contact')}>
 							<Link href='/contact' className="flex items-center -mb-1">
-								<span className="">
-									{isFrench ? 'Contact' : 'Contact'}
-								</span>
+								<span className="">{strings.contact}</span>
 							</Link>
 						</li>
-
 					</ul>
 					<button onClick={toggleMenu} className="flex justify-end p-4 md:hidden">
 						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
@@ -61,29 +51,43 @@ export const Navigation: React.FC<Props> = ({ active, locale }) => {
 						</svg>
 					</button>
 				</div>
-				{
-					isActive && (
-						<div className="flex flex-col text-center mx-auto">
-							<ul className={"space-y-4 md:flex"}>
-								<li className={activeLink('pricing')}>
-									<Link href='/pricing' className="flex">
-										<span className="flex">
-											{isFrench ? 'Coûts' : 'Pricing'}
-										</span>
-									</Link>
-								</li>
-								<li className={activeLink('contact')}>
-									<Link href='/contact' className="flex">
-										<span className="flex">
-											{isFrench ? 'Contact' : 'Contact'}
-										</span>
-									</Link>
-								</li>
-							</ul>
-						</div>
-					)
-				}
+				{isActive && (
+					<div className="flex flex-col text-center mx-auto">
+						<ul className="space-y-4 md:flex">
+							<li className={activeLink('pricing')}>
+								<Link href='/pricing' className="flex">
+									<span className="flex">{strings.pricing}</span>
+								</Link>
+							</li>
+							<li className={activeLink('contact')}>
+								<Link href='/contact' className="flex">
+									<span className="flex">{strings.contact}</span>
+								</Link>
+							</li>
+						</ul>
+					</div>
+				)}
 			</div>
 		</nav>
 	)
 }
+
+const useStrings = (locale: string) => {
+	const isFrench = locale.toLowerCase().includes('fr');
+	return {
+		en: {
+			backToHomepage: "Back to Gesmax by Bogital homepage",
+			logoAlt: "Gesmax by Bogital Logo",
+			pricing: "Pricing",
+			contact: "Contact"
+		},
+		fr: {
+			backToHomepage: "Retour à la page d'accueil de Gesmax by Bogital",
+			logoAlt: "Logo de Gesmax by Bogital",
+			pricing: "Coûts",
+			contact: "Contact"
+		}
+	}[isFrench ? 'fr' : 'en'];
+}
+
+export default Navigation
